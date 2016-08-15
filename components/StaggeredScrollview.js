44830ab7539c45df20884ba5488fb8dc324b2862
Zhi Rui Du <zhirui@Clintons-MacBook-Pro-2.local>
2016-08-15 14:24:17 -0400

    right now only a modal pops up

--- components/StaggeredScrollview.js
+++ components/StaggeredScrollview.js
@@ -3,7 +3,7 @@
  */
 
 import React, {Component} from 'react';
-import {Animated, ScrollView, View, Text, StyleSheet, Dimensions, Easing, Image} from 'react-native';
+import {Animated, ScrollView, View, Text, StyleSheet, Dimensions, Easing, Image, TouchableWithoutFeedback, Modal} from 'react-native';
 
 import AnimatedWrapper from './AnimatedWrapper';
 
@@ -15,19 +15,21 @@ export default class StaggeredScrollview extends Component {
 
     this.state = {
       scrollOffset: 0,
+      isExpanded: false,
       fadeAnim: new Animated.Value(0),
       time: 750,
       //temp value, ususally, replaced when component mounts, right now only works with uniform/mostly uniform rows
       rowHeight: 200,
       screenSpace: height,
-      animatedValues: [],
+      styleValues: [],
       visibleAnimations: [],
       interpolatedValues: [],
       scalingValues: [],
       rotateValues: [],
       alreadyAnimated: 0,
       endFade: new Animated.Value(0),
-      interred: 0
+      interred: 0,
+      modal: false
     }
 
     this.state.interred = this.state.endFade.interpolate({
@@ -59,7 +61,7 @@ export default class StaggeredScrollview extends Component {
     console.log(this.state.scrollOffset);
     while (((this.state.alreadyAnimated - 1) * this.state.rowHeight) <= (this.state.screenSpace + this.state.scrollOffset)) {
       this.state.visibleAnimations.push(Animated.timing(
-        this.state.animatedValues[this.state.alreadyAnimated - 1],
+        this.state.styleValues[this.state.alreadyAnimated - 1].animated,
         {
           fromValue: 0,
           toValue: 1,
@@ -87,10 +89,10 @@ export default class StaggeredScrollview extends Component {
 
     // doesn't really need to use map, could just do with a for loop
     // it just creates animations and maps them to the animated values array
-    this.state.animatedValues.map((value, key) => {
+    this.state.styleValues.map((value, key) => {
       if (((key + 1) * this.state.rowHeight) <= (this.state.screenSpace + this.state.scrollOffset)) {
         this.state.visibleAnimations.push(Animated.timing(
-          this.state.animatedValues[key],
+          this.state.styleValues[key].animated,
           {
             fromValue: 0,
             toValue: 1,
@@ -124,15 +126,45 @@ export default class StaggeredScrollview extends Component {
             scrollEventThrottle={100}
             showsVerticalScrollIndicator={false}>
             {this.props.children.map((child, key) => {
-              this.state.animatedValues.push(new Animated.Value(0));
-              this.state.interpolatedValues.push(this.state.animatedValues[key].interpolate({
+              this.state.styleValues.push(
+                {
+                  isExpanded: false,
+                  animated: new Animated.Value(0),
+                  scaling: new Animated.Value(1),
+                  position: 'relative',
+                  isModalVisible: false
+                }
+              );
+              this.state.styleValues[key].translateX = this.state.styleValues[key].animated.interpolate({
                 inputRange: [0, 1],
                 outputRange: [-50, 0]
-              }));
-              return (
-                <Animated.View style={{opacity: this.state.animatedValues[key], transform: [{translateX: this.state.interpolatedValues[key]}]}}>
+              });
 
+              return (
+                <Animated.View style={{
+                  opacity: this.state.styleValues[key].animated,
+                  position: this.state.styleValues[key].position,
+                  alignSelf: 'center',
+                  transform: [{translateX: this.state.styleValues[key].translateX}]
+                }} onLayout={ ({nativeEvent: {layout : {width: width, height: height}}}) => {
+                console.log('hello this was ran');
+                  this.state.styleValues[key].height = this.state.styleValues[key].scaling.interpolate({
+                    inputRange: [1, 2],
+                    outputRange: [height, this.state.screenSpace]
+                  });
+                  }}>
+                  <Modal
+                    animationType="fade"
+                    transparent={false}
+                  visible={this.state.styleValues[key].isModalVisible}>
+                   <View style={{backgroundColor: 'black', height: height, width: width, justifyContent: 'center'}}>
+                     <TouchableWithoutFeedback onPress={() => {this.state.styleValues[key].isModalVisible = false; this.forceUpdate()}}>
+                     {child}
+                   </TouchableWithoutFeedback></View>
+                    </Modal>
+                  <TouchableWithoutFeedback onPress={() => {this.state.styleValues[key].isModalVisible = true; this.forceUpdate()}}>
                   {child}
+                    </TouchableWithoutFeedback>
                 </Animated.View>
               );})
             }
